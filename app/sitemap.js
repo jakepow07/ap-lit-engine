6687174326c60616e0cd5e3471572a9cdc232df9
jakepow07 <65188545+jakepow07@users.noreply.github.com>
2026-04-15 23:14:16 -0400

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://your-site.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
